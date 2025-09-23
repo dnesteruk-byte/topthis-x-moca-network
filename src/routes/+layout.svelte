52075e8bf7f38493e3f3c529d52c3b836d352fa3
Dmytro Nesteruk <my.web.acc.dn@gmail.com>
2025-09-23 22:05:46 +0300

    chore: initial commit;

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "@shared/styles/global.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>TopThis x Moca Network</title>
+</svelte:head>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	{@render children?.()}
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      "@pages/*": "./src/pages/*",
+      "@widgets/*": "./src/widgets/*",
+      "@features/*": "./src/features/*",
+      "@entities/*": "./src/enitities/*",
+      "@shared/*": "./src/shared/*",
+    }
+  }
+};
+
+export default config;
